55753d3f315edccabc645a2bb5f96b8cf39d0a1e
tommaso <tommaso.burlon@github.com>
2022-02-09 10:27:06 +0100

    Now for every tomcat server there is a single Erlang node

--- AuctionHandler/AuctionHandlerWebApp/src/main/java/communication/OtpMboxSingleton.java
+++ AuctionHandler/AuctionHandlerWebApp/src/main/java/communication/OtpMboxSingleton.java
@@ -5,20 +5,31 @@ import com.ericsson.otp.erlang.OtpNode;
 
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.net.InetAddress;
 
 public class OtpMboxSingleton {
     private static final String cookie = "abcde";
-
-    private OtpMbox otpMbox = null;
+    private static OtpNode otpNode = null;
+    private OtpMbox otpMbox;
 
     private OtpMboxSingleton(String id) throws IOException{
-        OtpNode otpNode = new OtpNode(id + "@localhost", cookie);
-        otpMbox =  otpNode.createMbox(id);
+        otpMbox = otpNode.createMbox(id);
         System.out.println("OtpMBOX name: " + otpMbox.getName() + ", otpMBOX pid" + otpMbox.self().toString());
     }
 
     public static OtpMbox getInstance(HttpSession session) {
+        if(otpNode == null){
+            try {
+                //TODO: find an appropriate name for the "client" Erlang node
+                String nodeName = InetAddress.getLocalHost().getHostName();
+                otpNode = new OtpNode(nodeName + "@localhost", cookie);
+            } catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
         OtpMboxSingleton ret = (OtpMboxSingleton) session.getAttribute("otpmbox");
+
         if (ret == null) {
             try {
                 ret = new OtpMboxSingleton(session.getId());
@@ -27,7 +38,7 @@ public class OtpMboxSingleton {
                 e.printStackTrace();
             }
         }
+
         return ret.otpMbox;
     }
-
 }
